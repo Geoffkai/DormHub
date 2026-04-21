d9b1415bc9cd88fb4560545877e75065d766b345
Geoffrey Tomagan <tomagan.geoffreyadam@gmail.com>
2026-04-22 02:47:58 +0800

    Migrate Swing GUI into main app and remove JavaFX setup

--- src/main/java/com/dormhub/Main.java
+++ src/main/java/com/dormhub/Main.java
@@ -1,29 +1,12 @@
 package com.dormhub;
 
-import com.dormhub.cli.CLIApp;
 import java.util.Arrays;
 import java.util.Scanner;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-public class Main extends Application {
+import com.dormhub.cli.CLIApp;
+import com.dormhub.view.GUIMain;
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/app.fxml"));
-        Scene scene = new Scene(loader.load(), 420, 260);
-
-        var css = Main.class.getResource("/app.css");
-        if (css != null) {
-            scene.getStylesheets().add(css.toExternalForm());
-        }
-
-        stage.setTitle("DormHub");
-        stage.setScene(scene);
-        stage.show();
-    }
+public class Main {
 
     public static void main(String[] args) {
         boolean cliMode = Arrays.stream(args).anyMatch("--cli"::equalsIgnoreCase);
@@ -37,7 +20,6 @@ public class Main extends Application {
             return;
         }
 
-        launch(args);
+        GUIMain.main(new String[0]);
     }
 }
-
